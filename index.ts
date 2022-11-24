2077627048c8c3228bc766a24f7051ec3113f7a0
Andrew Leith <andrew.leith@cds-snc.ca>
2022-11-24 01:59:10 +0000

    I dont think we need this since we are detecting all snakes' positions (including our own) below.

--- index.ts
+++ index.ts
@@ -54,18 +54,18 @@ function move(gameState: GameState): MoveResponse {
   const myHead = gameState.you.body[0];
   const myNeck = gameState.you.body[1];
 
-  if (myNeck.x < myHead.x) {        // Neck is left of head, don't move left
-    isMoveSafe.left = false;
+  // if (myNeck.x < myHead.x) {        // Neck is left of head, don't move left
+  //   isMoveSafe.left = false;
 
-  } else if (myNeck.x > myHead.x) { // Neck is right of head, don't move right
-    isMoveSafe.right = false;
+  // } else if (myNeck.x > myHead.x) { // Neck is right of head, don't move right
+  //   isMoveSafe.right = false;
 
-  } else if (myNeck.y < myHead.y) { // Neck is below head, don't move down
-    isMoveSafe.down = false;
+  // } else if (myNeck.y < myHead.y) { // Neck is below head, don't move down
+  //   isMoveSafe.down = false;
 
-  } else if (myNeck.y > myHead.y) { // Neck is above head, don't move up
-    isMoveSafe.up = false;
-  }
+  // } else if (myNeck.y > myHead.y) { // Neck is above head, don't move up
+  //   isMoveSafe.up = false;
+  // }
 
   // TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
 
